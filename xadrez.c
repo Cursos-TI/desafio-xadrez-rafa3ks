e1eb0e6b27141fef610b7ef58afe49e28383b476
rafa3ks <rafacarvalho231@gmail.com>
2025-03-17 23:39:34 +0000

    correção de erros

--- xadrez.c
+++ xadrez.c
@@ -2,31 +2,37 @@
 
 int main() {
 
-    int movimentoT;
+    int i;
     int movimentoB=1;
     int movimentoRA=0;
 
 
     //ultilização do FOR para movimentação da torre
     printf("Movimentação da torre:")
-    for(movimentoT=1;movimentoT<=5;movimentoT++){
-        printf("Torre se moveu: %dX para direita \n", movimentoT);
+    for (i=1;i<=5;i++)
+    {
+        printf("Torre se moveu: %dX para direita \n", i);
     }
+
     
     printf("  \n");
 
     //ultilização do WHILE para movimentação do bispo
-    While(movimentoB<=5){
+    while (movimentoB<=5)
+    {
         printf("Bispo se moveu: %dX para Cima,Direita \n", movimentoB);
         movimentoB++
     }
 
+
     printf("  \n");
 
-    do{
+    do
+    {
         printf("Rainha e moveu: %dX para esquerda \n", movimentoRA);
         movimentoRA++
-    }While(movimentoRA<=8);
+    } while (movimentoRA<=8);
 
+    
     return 0;
 }
